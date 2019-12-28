32e557ad06ee95ef12754ee9daff0bfe2d33578b
VARADHARAJAN <svaradharajan768@gmail.com>
2019-12-28 20:49:31 +0530

    Final commit for doubly linked list

--- src/datastructures/doubly_linked_list.go
+++ src/datastructures/doubly_linked_list.go
@@ -1,9 +1,21 @@
 package datastructures
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
 type DoubleLinkedNode struct {
 	Data int
 	Prev *DoubleLinkedNode
 	Next *DoubleLinkedNode
+}
+
+type DoublyLinkedList struct {
+	Head *DoubleLinkedNode
+	Tail *DoubleLinkedNode
+	size int
 }
 
 func CreateDoubleLinkNode(data int) DoubleLinkedNode {
@@ -12,4 +24,188 @@ func CreateDoubleLinkNode(data int) DoubleLinkedNode {
 		Prev: nil,
 		Next: nil,
 	}
+}
+
+func CreateDoublyLinkedList() DoublyLinkedList {
+	return DoublyLinkedList{
+		Head: nil,
+		Tail: nil,
+	}
+}
+
+func (d *DoublyLinkedList) Append(data int) {
+	node := CreateDoubleLinkNode(data)
+	if d.Head == nil {
+		d.Head = &node
+		d.Tail = &node
+		d.size++
+		return
+	} else {
+		node.Prev = d.Tail
+		d.Tail.Next = &node
+		d.Tail = &node
+		d.size++
+	}
+}
+
+func (d *DoublyLinkedList) ToString() string {
+	currentNode := d.Head
+	printableString := ""
+	for {
+		if currentNode == nil {
+			return strings.Trim(printableString, " ")
+		}
+		printableString += strconv.Itoa(currentNode.Data) + " "
+		currentNode = currentNode.Next
+
+	}
+}
+
+func (d *DoublyLinkedList) Size() int {
+	return d.size
+}
+
+func (d *DoublyLinkedList) IsEmpty() bool {
+	return d.Size() == 0
+}
+
+func (d *DoublyLinkedList) Clear() {
+	d.Head = nil
+	d.Tail = nil
+	d.size = 0
+}
+
+func (d *DoublyLinkedList) AddAt(index int, data int) {
+	nodeToBeInserted := CreateDoubleLinkNode(data)
+	if index <= d.Size()/2 {
+		//search from the head
+		currentNode := d.Head
+		currentPosition := 1
+		for {
+			if currentNode == nil {
+				break
+			}
+			if currentPosition+1 == index {
+				currentNode.Next.Prev = &nodeToBeInserted
+				nodeToBeInserted.Next = currentNode.Next
+				currentNode.Next = &nodeToBeInserted
+				nodeToBeInserted.Prev = currentNode
+				d.size++
+			}
+			currentNode = currentNode.Next
+			currentPosition++
+		}
+	} else {
+		// search from the end
+		currentNode := d.Tail
+		currentPosition := d.Size()
+		for {
+			if currentNode == nil {
+				break
+			}
+			if currentPosition == index {
+				previousNode := currentNode.Prev
+				previousNode.Next = &nodeToBeInserted
+				nodeToBeInserted.Prev = previousNode
+				nodeToBeInserted.Next = currentNode
+				currentNode.Prev = &nodeToBeInserted
+				d.size++
+			}
+			currentNode = currentNode.Prev
+			currentPosition--
+		}
+	}
+
+}
+
+func (d *DoublyLinkedList) PeekFirst() int {
+	return d.Head.Data
+}
+
+func (d *DoublyLinkedList) PeekLast() int {
+	return d.Tail.Data
+}
+
+func (d *DoublyLinkedList) RemoveAt(index int) error {
+
+	if index <= d.Size()/2 {
+		currentNode := d.Head
+		currentPosition := 1
+		for {
+			if currentNode == nil {
+				return errors.New("Index out of bounds")
+			}
+			if currentPosition+1 == index {
+				currentNode.Next = currentNode.Next.Next
+				if currentNode.Next.Next != nil {
+					currentNode.Next.Next.Prev = currentNode
+				}
+				d.size--
+				return nil
+			}
+			currentNode = currentNode.Next
+			currentPosition++
+		}
+	} else {
+		currentNode := d.Tail
+		currentPosition := d.Size()
+		for {
+			if currentNode == nil {
+				return errors.New("Index out of bounds")
+			}
+			if currentPosition == index {
+				currentNode.Prev.Next = currentNode.Next
+				currentNode.Next.Prev = currentNode.Prev
+				d.size--
+				return nil
+			}
+			currentNode = currentNode.Prev
+			currentPosition--
+		}
+	}
+
+}
+
+func (d *DoublyLinkedList) RemoveFirst() error {
+	if d.Head == nil {
+		return errors.New("List not initialised")
+	}
+	d.Head = d.Head.Next
+	d.Head.Prev = nil
+	d.size--
+	return nil
+}
+
+func (d *DoublyLinkedList) RemoveLast() error {
+	if d.Tail == nil {
+		return errors.New("List not initialised")
+	}
+	d.Tail = d.Tail.Prev
+	d.Tail.Next = nil
+	d.size--
+	return nil
+}
+
+func (d *DoublyLinkedList) IndexOf(data int) int {
+	if d.Head == nil {
+		return -1
+	}
+	currentNode := d.Head
+	currentIndex := 1
+	for {
+		if currentNode == nil {
+			break
+		}
+		if currentNode.Data == data {
+			return currentIndex
+		}
+		currentNode = currentNode.Next
+		currentIndex++
+	}
+
+	return -1
+}
+
+func (d *DoublyLinkedList) Contains(data int) bool {
+	return d.IndexOf(data) != -1
 }
--- src/datastructures/doubly_linked_list_test.go
+++ src/datastructures/doubly_linked_list_test.go
@@ -1,6 +1,8 @@
 package datastructures
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIfDoubleLinkNodeIsCreated(t *testing.T) {
 	node := CreateDoubleLinkNode(1)
@@ -11,6 +13,256 @@ func TestIfDoubleLinkNodeIsCreated(t *testing.T) {
 		t.FailNow()
 	}
 	if node.Next != nil {
+		t.FailNow()
+	}
+}
+
+func TestIfEmptyDoublyLinkedListWorks(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	if list.Head != nil {
+		t.FailNow()
+	}
+	if list.Tail != nil {
+		t.FailNow()
+	}
+	if list.Size() != 0 {
+		t.FailNow()
+	}
+	if !list.IsEmpty() {
+		t.FailNow()
+	}
+}
+
+func TestIfAppendingToListWorks(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	if list.ToString() != "1 2 3" {
+		t.FailNow()
+	}
+	if list.Size() != 3 {
+		t.FailNow()
+	}
+	if list.IsEmpty() {
+		t.FailNow()
+	}
+}
+
+func TestIfClearingTheListWorks(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Clear()
+	if !list.IsEmpty() {
+		t.FailNow()
+	}
+	if "" != list.ToString() {
+		t.FailNow()
+	}
+
+}
+
+func TestIfAddAtIndexWorksCorrectly_dll_first_half(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.AddAt(2, 5)
+	if "1 5 2 3 4" != list.ToString() {
+		t.FailNow()
+	}
+	if 5 != list.Size() {
+		t.FailNow()
+	}
+}
+
+func TestIfAddAtIndexWorksCorrectly_dll_second_half(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	list.Append(6)
+	list.Append(7)
+	list.Append(8)
+	list.Append(9)
+	list.AddAt(7, 10)
+	if "1 2 3 4 5 6 10 7 8 9" != list.ToString() {
+		t.FailNow()
+	}
+	if 10 != list.Size() {
+		t.FailNow()
+	}
+}
+
+func TestIfPeekFirstWorksCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	if 1 != list.PeekFirst() {
+		t.FailNow()
+	}
+}
+
+func TestIfPeekLastWorksCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	if 5 != list.PeekLast() {
+		t.FailNow()
+	}
+}
+
+func TestIfRemoveAtNonBoundaryIndexInFirsthalfWorksCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	list.Append(6)
+	list.Append(7)
+	list.RemoveAt(3)
+	if "1 2 4 5 6 7" != list.ToString() {
+		t.FailNow()
+	}
+}
+
+func TestIfRemovalAtInvalidIndexGreaterBoundsThrowsException(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	list.Append(6)
+	list.Append(7)
+	err := list.RemoveAt(10)
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestIfRemoveAtNonBoundaryIndexInSecondhalfWorksCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	list.Append(6)
+	list.Append(7)
+	list.RemoveAt(6)
+	if "1 2 3 4 5 7" != list.ToString() {
+		t.FailNow()
+	}
+	if 6 != list.Size() {
+		t.FailNow()
+	}
+}
+
+func TestIfRemovalAtInvalidIndexLowerBoundsThrowsException(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.Append(4)
+	list.Append(5)
+	list.Append(6)
+	list.Append(7)
+	err := list.RemoveAt(-1)
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestIfRemovalAtBeginningWorksCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.RemoveFirst()
+	if "2 3" != list.ToString() {
+		t.FailNow()
+	}
+	if 2 != list.Size() {
+		t.FailNow()
+	}
+}
+
+func TestIfRemovalAtEndWorksCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	list.RemoveLast()
+	if "1 2" != list.ToString() {
+		t.FailNow()
+	}
+	if 2 != list.Size() {
+		t.FailNow()
+	}
+}
+
+func TestIfIndexOfElementIsFoundCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	if 2 != list.IndexOf(2) {
+		t.FailNow()
+	}
+}
+
+func TestIfIndexOfInvalidElementIsHandledCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	if -1 != list.IndexOf(4) {
+		t.FailNow()
+	}
+}
+
+func TestIfRemoveAtEndWorksOnEmptyDoubleLinkList(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	err := list.RemoveLast()
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestIfRemoveAtBiginningWorksOnEmptyDoubleLinkList(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	err := list.RemoveFirst()
+	if err == nil {
+		t.FailNow()
+	}
+}
+
+func TestIfIndexOfInvalidElementInEmptyListIsHandledCorrectly(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	if -1 != list.IndexOf(4) {
+		t.FailNow()
+	}
+}
+
+func TestIfContainsForTrueWorks(t *testing.T) {
+	list := CreateDoublyLinkedList()
+	list.Append(1)
+	list.Append(2)
+	list.Append(3)
+	if !list.Contains(2) {
 		t.FailNow()
 	}
 }
