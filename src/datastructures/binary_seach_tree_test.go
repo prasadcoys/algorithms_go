2784f9ee73cb4d9eec1789efcb4d76a53693c5f2
VARADHARAJAN <svaradharajan768@gmail.com>
2020-02-12 23:04:57 +0530

    Added Add and contains logic

--- src/datastructures/binary_seach_tree_test.go
+++ src/datastructures/binary_seach_tree_test.go
@@ -35,14 +35,118 @@ func TestIfNodeCanBeAddedToAnEmptyBinarySearchTree(t *testing.T) {
 	}
 }
 
-func TestIfNodeCanBeAddedToAnNonEmptyBinarySearchTree(t *testing.T) {
+func TestIfNodeCanBeAddedToRootAnNonEmptyBinarySearchTree(t *testing.T) {
 	binaryTree := BinarySearchTree{}
 	binaryTree.Add(&BSTNode{data: 23})
 	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+
 	if binaryTree.root.right.data != 30 {
 		t.Fail()
 	}
-	if binaryTree.root.left != nil {
+	if binaryTree.root.left.data != 15 {
 		t.Fail()
 	}
+	if binaryTree.root.data != 23 {
+		t.Fail()
+	}
+}
+
+func TestIfNodeCanBeAddedToTheLeftOfALeftSubTree(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 12})
+	if binaryTree.root.left.left.data != 12 {
+		t.Fail()
+	}
+}
+
+func TestIfNodeCanBeAddedToTheRightOfARightSubTree(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 35})
+	if binaryTree.root.right.right.data != 35 {
+		t.Fail()
+	}
+}
+
+func TestIfNodeCanBeAddedToTheRightOfALeftSubTree(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 18})
+	if binaryTree.root.left.right.data != 18 {
+		t.Fail()
+	}
+}
+
+func TestIfNodeCanBeAddedToTheLeftOfARightSubTree(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 25})
+	if binaryTree.root.right.left.data != 25 {
+		t.Fail()
+	}
+}
+
+func TestIfTreeContainsNodeWorksForAPresentValue(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 25})
+	if binaryTree.Contains(15) != true {
+		t.Fail()
+	}
+}
+
+func TestIfTreeContainsNodeWorksForAnAbsentValue(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 25})
+	if binaryTree.Contains(16) != false {
+		t.Fail()
+	}
+}
+
+func TestIfTreeContainsNodeWorksForAValuePresentInRightSubtree(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 25})
+	if binaryTree.Contains(30) != true {
+		t.Fail()
+	}
+}
+
+func TestIfTreeContainsNodeWorksForAValueAbsentInRightSubtree(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	binaryTree.Add(&BSTNode{data: 23})
+	binaryTree.Add(&BSTNode{data: 30})
+	binaryTree.Add(&BSTNode{data: 15})
+	binaryTree.Add(&BSTNode{data: 25})
+	if binaryTree.Contains(50) != false {
+		t.Fail()
+	}
+}
+
+func TestIfTreeContainsReturnsFalseOnEmptyTree(t *testing.T) {
+	binaryTree := BinarySearchTree{}
+	if binaryTree.Contains(25) != false {
+		t.Fail()
+	}
+}
+
+func TestIfSizeOfEmptyBinarySearchTreeReturnsZero(t *testing.T) {
+
 }
--- src/datastructures/binary_search_tree.go
+++ src/datastructures/binary_search_tree.go
@@ -13,5 +13,45 @@ type BinarySearchTree struct {
 func (b *BinarySearchTree) Add(node *BSTNode) {
 	if b.root == nil {
 		b.root = node
+		return
+	}
+	b.addNode(node, b.root)
+
+}
+
+func (b *BinarySearchTree) addNode(node *BSTNode, currentRoot *BSTNode) {
+	if node.data < currentRoot.data {
+		if currentRoot.left == nil {
+			currentRoot.left = node
+		} else {
+			b.addNode(node, currentRoot.left)
+		}
+
+	} else if node.data > currentRoot.data {
+		if currentRoot.right == nil {
+			currentRoot.right = node
+		} else {
+			b.addNode(node, currentRoot.right)
+		}
+	}
+}
+
+func (b *BinarySearchTree) Contains(value int) bool {
+	if b.root == nil {
+		return false
+	} else {
+		return b.containsNode(value, b.root)
+	}
+}
+
+func (b *BinarySearchTree) containsNode(value int, currentRoot *BSTNode) bool {
+	if currentRoot == nil {
+		return false
+	} else if value == currentRoot.data {
+		return true
+	} else if value < currentRoot.data {
+		return b.containsNode(value, currentRoot.left)
+	} else {
+		return b.containsNode(value, currentRoot.right)
 	}
 }
