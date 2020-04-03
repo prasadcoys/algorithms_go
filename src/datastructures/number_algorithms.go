1dc8502df0a9ed32df14d37e86befaf951ef7189
VARADHARAJAN <svaradharajan768@gmail.com>
2020-04-04 00:45:29 +0530

    atoi solution

--- src/datastructures/number_algorithms.go
+++ src/datastructures/number_algorithms.go
@@ -1,5 +1,9 @@
 package datastructures
 
+import (
+	"math"
+)
+
 func ReverseNumber(number int) int {
 	reversedNumber := 0
 	for {
@@ -13,4 +17,43 @@ func ReverseNumber(number int) int {
 		return 0
 	}
 	return reversedNumber
+}
+
+func AtoI(text string) int {
+
+	isNegative := false
+	numbersMap := make(map[int]int)
+	numbersMap[45] = -1
+	number := 0
+
+	j := 0
+	for i := 48; i <= 57; i++ {
+		numbersMap[i] = j
+		j = j + 1
+	}
+
+	for _, c := range text {
+		if c == 32 || c == 43 {
+			continue
+		}
+		numberInInteger, ok := numbersMap[int(c)]
+		if !ok {
+			break
+		}
+		if numberInInteger < 0 {
+			isNegative = true
+			continue
+		}
+		number = (number * 10) + numberInInteger
+	}
+	if isNegative {
+		if number*-1 < math.MinInt32 {
+			return math.MinInt32
+		}
+		return number * -1
+	}
+	if number > math.MaxInt32 {
+		return math.MaxInt32
+	}
+	return number
 }
--- src/datastructures/number_problems_test.go
+++ src/datastructures/number_problems_test.go
@@ -20,3 +20,43 @@ func TestIfReversingOfNegativeNumberWorksCorrectly(t *testing.T) {
 	number := -123
 	assert.Equal(t, -321, ReverseNumber(number))
 }
+
+func TestConvertStringToIntegerWorks(t *testing.T) {
+	text := "42"
+	assert.Equal(t, 42, AtoI(text))
+}
+
+func TestConvertStringWithSpaceToIntegerWorks(t *testing.T) {
+	text := "  42"
+	assert.Equal(t, 42, AtoI(text))
+}
+
+func TestIfConversionOfNegativeNumberWorks(t *testing.T) {
+	text := "-42"
+	assert.Equal(t, -42, AtoI(text))
+}
+
+func TestIfAnyAdditionalStringAfterNumberIsIgnored(t *testing.T) {
+	text := "-42 and some text"
+	assert.Equal(t, -42, AtoI(text))
+}
+
+func TestIfTextBeforeNumberReturnsZero(t *testing.T) {
+	text := "some txt and number"
+	assert.Equal(t, 0, AtoI(text))
+}
+
+func TestIfOvershootingOfValuesResultInMaxValue(t *testing.T) {
+	text := "-91283472332"
+	assert.Equal(t, -2147483648, AtoI(text))
+}
+
+func TestIfNegativeNumberStringWithSpaceIsCorrectlyTransformed(t *testing.T) {
+	text := "   -42"
+	assert.Equal(t, -42, AtoI(text))
+}
+
+func TestIfPositiveNumberStringWithPlusSignIsCorrectlyTransformed(t *testing.T) {
+	text := "+42"
+	assert.Equal(t, 42, AtoI(text))
+}
